9e6176d1fa47d69487f789895a984b653eefc3d5
Darko875 <danielviana18@gmail.com>
2020-12-04 14:28:04 +0000

    Hospitals requests added - 0.0.5

--- backend/src/controllers/ContractsController.js
+++ backend/src/controllers/ContractsController.js
@@ -52,30 +52,6 @@ module.exports = {
     
         return res.json(valor_2011[0])
     },
-    async search_max_companies (req,res){
-        let companies_max = await db.query("SELECT COUNT(`adjudicatario`) AS count, group_concat(DISTINCT `adjudicatario`) FROM `informacao` WHERE `adjudicante` LIKE '%Hospital%' GROUP BY `adjudicatario` ORDER BY count DESC LIMIT 25")
     
-        return res.json(companies_max[0])
-    },
-    async search_max_2008_companies (req,res){
-        let companies_max_2008 = await db.query("SELECT COUNT(`adjudicatario`) AS count, group_concat(DISTINCT `adjudicatario`) FROM `informacao` WHERE `adjudicante` LIKE '%Hospital%' AND `dataPub` BETWEEN '2008-01-01' AND '2008-12-31' GROUP BY `adjudicatario` ORDER BY count DESC")
-    
-        return res.json(companies_max_2008[0])
-    },
-    async search_max_2009_companies (req,res){
-        let companies_max_2009 = await db.query("SELECT COUNT(`adjudicatario`) AS count, group_concat(DISTINCT `adjudicatario`) FROM `informacao` WHERE `adjudicante` LIKE '%Hospital%' AND `dataPub` BETWEEN '2009-01-01' AND '2009-12-31' GROUP BY `adjudicatario` ORDER BY count DESC")
-    
-        return res.json(companies_max_2009[0])
-    },
-    async search_max_2010_companies (req,res){
-        let companies_max_2010 = await db.query("SELECT COUNT(`adjudicatario`) AS count, group_concat(DISTINCT `adjudicatario`) FROM `informacao` WHERE `adjudicante` LIKE '%Hospital%' AND `dataPub` BETWEEN '2010-01-01' AND '2010-12-31' GROUP BY `adjudicatario` ORDER BY count DESC")
-    
-        return res.json(companies_max_2010[0])
-    },
-    async search_max_2011_companies (req,res){
-        let companies_max_2011 = await db.query("SELECT COUNT(`adjudicatario`) AS count, group_concat(DISTINCT `adjudicatario`) FROM `informacao` WHERE `adjudicante` LIKE '%Hospital%' AND `dataPub` BETWEEN '2011-01-01' AND '2011-12-31' GROUP BY `adjudicatario` ORDER BY count DESC")
-    
-        return res.json(companies_max_2011[0])
-    },
 
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const routes = express.Router()
 const ProfileController = require('./controllers/ProfileController')
 const ContractsController = require('./controllers/ContractsController')
 const CompaniesController = require('./controllers/CompaniesControllers')
+const HospitalsController = require('./controllers/HospitalsController')
 
 
 
@@ -24,17 +25,30 @@ routes.get('/contract-max-2010', ContractsController.search_max_2010)
 routes.get('/contracts-sum-2011', ContractsController.search_sum_2011)
 routes.get('/contract-max-2011', ContractsController.search_max_2011)
 
-
-routes.get('/contract-number-2008', ContractsController.search_max_2008_companies)
-routes.get('/contract-number-2009', ContractsController.search_max_2009_companies)
-routes.get('/contract-number-2010', ContractsController.search_max_2010_companies)
-routes.get('/contract-number-2011', ContractsController.search_max_2011_companies)
+routes.get('/contract-number', CompaniesController.search_max_companies)
+routes.get('/contract-number-2008', CompaniesController.search_max_2008_companies)
+routes.get('/contract-number-2009', CompaniesController.search_max_2009_companies)
+routes.get('/contract-number-2010', CompaniesController.search_max_2010_companies)
+routes.get('/contract-number-2011', CompaniesController.search_max_2011_companies)
 
 
 routes.get('/search-top-10-companies', CompaniesController.search_top_10_companies)
 routes.get('/search-top-10-companies-2008', CompaniesController.search_top_10_companies_2008)
 routes.get('/search-top-10-companies-2009', CompaniesController.search_top_10_companies_2009)
 routes.get('/search-top-10-companies-2010', CompaniesController.search_top_10_companies_2010)
-routes.get('/search-top-10-companies-2011', CompaniesController.search_top_10_companies_2010)
+routes.get('/search-top-10-companies-2011', CompaniesController.search_top_10_companies_2011)
+
+routes.get('/search-top-10-hospitals', HospitalsController.search_top_10_hospitals)
+routes.get('/search-top-10-hospitals-2008', HospitalsController.search_top_10_hospitals_2008)
+routes.get('/search-top-10-hospitals-2009', HospitalsController.search_top_10_hospitals_2009)
+routes.get('/search-top-10-hospitals-2010', HospitalsController.search_top_10_hospitals_2010)
+routes.get('/search-top-10-hospitals-2011', HospitalsController.search_top_10_hospitals_2011)
+
+routes.get('/search-top-10-minus-hospitals', HospitalsController.search_top_10_minus_hospitals)
+routes.get('/search-top-10-minus-hospitals-2008', HospitalsController.search_top_10_minus_hospitals_2008)
+routes.get('/search-top-10-minus-hospitals-2009', HospitalsController.search_top_10_minus_hospitals_2009)
+routes.get('/search-top-10-minus-hospitals-2010', HospitalsController.search_top_10_minus_hospitals_2010)
+routes.get('/search-top-10-minus-hospitals-2011', HospitalsController.search_top_10_minus_hospitals_2011)
+
 
 module.exports = routes;
