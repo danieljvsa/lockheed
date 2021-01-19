c18d83da4aa7ccefcc85175c735ebb722cb681e1
Darko875 <danielviana18@gmail.com>
2021-01-19 14:47:17 +0000

    Back to older version

--- backend/src/controllers/SuspectContractsController.js
+++ backend/src/controllers/SuspectContractsController.js
@@ -3,7 +3,7 @@ const {db} = require('../database/connection')
 
 module.exports = {
     async public_works_contracts (req,res){
-        let contracts = await db.query("SELECT * FROM `informacao` WHERE `adjudicante` LIKE '%Hospital%' AND `adjudicante` NOT LIKE '%câmara%' AND adjudicante NOT LIKE '%município%' AND `dataPub` BETWEEN '2008-01-01' AND '2011-12-31' AND tipoProc LIKE '%Ajuste direto%' AND tipoCont like '%empreitadas de obras públicas%' AND pContr < 30000")
+        let contracts = await db.query("SELECT * FROM `informacao` WHERE `adjudicante` LIKE '%Hospital%' AND `adjudicante` NOT LIKE '%câmara%' AND adjudicante NOT LIKE '%município%' AND `dataPub` BETWEEN '2008-01-01' AND '2011-12-31' AND tipoProc LIKE '%Ajuste direto%' AND tipoCont like '%empreitadas de obras públicas%' AND pContr > 30000")
         
         
 
@@ -12,7 +12,7 @@ module.exports = {
     },
 
     async acquisition_of_real_state(req,res){
-        let contracts = await db.query("SELECT * FROM informacao WHERE adjudicante LIKE '%hospital%' AND adjudicante NOT LIKE '%câmara%' AND adjudicante NOT LIKE '%município%' AND dataPub BETWEEN '2008-01-01' AND '2011-12-31' AND tipoProc LIKE '%Ajuste direto%' AND tipoCont like '%aquisição de bens móveis%' AND pContr < 20000")
+        let contracts = await db.query("SELECT * FROM informacao WHERE adjudicante LIKE '%hospital%' AND adjudicante NOT LIKE '%câmara%' AND adjudicante NOT LIKE '%município%' AND dataPub BETWEEN '2008-01-01' AND '2011-12-31' AND tipoProc LIKE '%Ajuste direto%' AND tipoCont like '%aquisição de bens móveis%' AND pContr > 20000")
         
         
 
@@ -20,7 +20,7 @@ module.exports = {
     },
 
     async purchasing_services(req,res){
-        let contracts = await db.query("SELECT * FROM informacao WHERE adjudicante LIKE '%hospital%' AND adjudicante NOT LIKE '%câmara%' AND adjudicante NOT LIKE '%município%' AND dataPub BETWEEN '2008-01-01' AND '2011-12-31' AND tipoProc LIKE '%Ajuste direto%' AND tipoCont like '%aquisição de serviços%' AND pContr < 20000")
+        let contracts = await db.query("SELECT * FROM informacao WHERE adjudicante LIKE '%hospital%' AND adjudicante NOT LIKE '%câmara%' AND adjudicante NOT LIKE '%município%' AND dataPub BETWEEN '2008-01-01' AND '2011-12-31' AND tipoProc LIKE '%Ajuste direto%' AND tipoCont like '%aquisição de serviços%' AND pContr > 20000")
         
         
 
@@ -28,7 +28,7 @@ module.exports = {
     },
 
     async other_contracts(req,res){
-        let contracts = await db.query("SELECT * FROM informacao WHERE adjudicante LIKE '%hospital%' AND adjudicante NOT LIKE '%câmara%' AND adjudicante NOT LIKE '%município%' AND dataPub BETWEEN '2008-01-01' AND '2011-12-31' AND tipoProc LIKE '%Ajuste direto%' AND tipoCont like '%-%' AND pContr < 50000")
+        let contracts = await db.query("SELECT * FROM informacao WHERE adjudicante LIKE '%hospital%' AND adjudicante NOT LIKE '%câmara%' AND adjudicante NOT LIKE '%município%' AND dataPub BETWEEN '2008-01-01' AND '2011-12-31' AND tipoProc LIKE '%Ajuste direto%' AND tipoCont like '%-%' AND pContr > 50000")
     
         
         
